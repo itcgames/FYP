f0b0b85256300e2482b2d746b4e7dce3419bfa11
MichaelBridgette <c00205948@itcarlow.ie>
2019-04-04 21:40:30 +0100

    refactored code

--- STRIP WEBAPP/objectDetector.py
+++ STRIP WEBAPP/objectDetector.py
@@ -1,4 +1,6 @@
-# test.py
+# Author: Michael Bridgette
+# Description: Script for performing the actual object detection on an image
+# Date of last edit: 04/04/2019
 
 import numpy as np
 import os
@@ -20,18 +22,9 @@ LABELS_LOC = os.getcwd() + "/" + "label_map.pbtxt"
 NUM_CLASSES = 2
 
 #######################################################################################################################
-#def main():
-print("starting program . . .")
 
-    # end if
 
-    # this next comment line is necessary to avoid a false PyCharm warning
-    # noinspection PyUnresolvedReferences
-if StrictVersion(tf.__version__) < StrictVersion('1.5.0'):
-    raise ImportError('Please upgrade your tensorflow installation to v1.5.* or later!')
-    # end if
-
-    # load a (frozen) TensorFlow model into memory
+# load a (frozen) TensorFlow model into memory
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
@@ -39,54 +32,43 @@ with detection_graph.as_default():
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
-        # end with
-    # end with
 
-    # Loading label map
-    # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+# Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts `1`, we know that this corresponds to `Negative`
 label_map = label_map_util.load_labelmap(LABELS_LOC)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
-                                                                use_display_name=True)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
-        # end if
-    # end for
-#testimg = open('test.jpg', 'rb')
-#image_read = testimg.read()
 
-#image_64_encode = base64.encodestring(image_read)
-#print(type(image_64_encode))
-
+# Takes b64 string, constructs an image, performs object detection, encode image back to b64 and return
+# a dictionary with the image, accuracy and result
 def Object_Detection(b64String):
     with detection_graph.as_default():
         with tf.Session(graph=detection_graph) as sess:
 
-            imgdata = base64.b64decode(b64String)
+            imgdata = base64.b64decode(b64String) # decode string
 
             theImage = Image.open(BytesIO(imgdata))
             b, g, r = theImage.split()
-            Testimage = Image.merge("RGB", (r, g, b))
+            Testimage = Image.merge("RGB", (r, g, b)) # fixing colours of the image
             image_np = np.array(Testimage)
 
-                # Definite input and output Tensors for detection_graph
+            # Definite input and output Tensors for detection_graph
             image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-                # Each box represents a part of the image where a particular object was detected.
+            # Each box represents a part of the image where a particular object was detected.
             detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-                # Each score represent how level of confidence for each of the objects.
-                # Score is shown on the result image, together with the class label.
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
             detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
             detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
             num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
             image_np_expanded = np.expand_dims(image_np, axis=0)
-                # Actual detection.
+            # Actual detection.
             (boxes, scores, classes, num) = sess.run(
                 [detection_boxes, detection_scores, detection_classes, num_detections],
                 feed_dict={image_tensor: image_np_expanded})
-                # Visualization of the results of a detection.
-
-
-
+            # Visualization of the results of a detection.
             vis_util.visualize_boxes_and_labels_on_image_array(image_np,
                                                                 np.squeeze(boxes),
                                                                 np.squeeze(classes).astype(np.int32),
@@ -97,53 +79,39 @@ def Object_Detection(b64String):
             buffered = BytesIO()
             im = Image.fromarray(image_np,"RGB")
             b, g, r = im.split()
-            image = Image.merge("RGB", (r, g, b))
+            image = Image.merge("RGB", (r, g, b)) # fixing colours of the image once again
             image.save(buffered, format="JPEG")
-            img_str = base64.b64encode(buffered.getvalue())
+            img_str = base64.b64encode(buffered.getvalue()) # encode the result image
+
+            # check for any detections before creating dictionary object
+            # if the accuracy is under 0.4 (40%) Im choosing to basically consider this as no detection at all as its quite a low score
             if len([category_index.get(value) for index,value in enumerate(classes[0]) if scores[0,index] > 0.4]) > 0:
                 theResults = {
                 'name': [category_index.get(value) for index,value in enumerate(classes[0]) if scores[0,index] > 0.4][0]["name"],
                 'accuracy': scores[0,0],
-                'image': img_str
+                'image': img_str.decode('UTF-8')
                 }
             else:
                 theResults = {
                 'name': "No Result Detected",
                 'accuracy': 0,
-                'image': img_str
+                'image': img_str.decode('UTF-8')
                 }
-            print("Finished Object Detection")
             return theResults
-            # end for
-        # end with
-    # end with
-    #Object_Detection(image_64_encode)
-# end main
 
-#Object_Detection(image_64_encode)
 #######################################################################################################################
 def checkIfNecessaryPathsAndFilesExist():
-    if not os.path.exists(TEST_IMAGE_DIR):
-        print('ERROR: TEST_IMAGE_DIR "' + TEST_IMAGE_DIR + '" does not seem to exist')
-        return False
-    # end if
-
-    # ToDo: check here that the test image directory contains at least one image
 
     if not os.path.exists(FROZEN_INFERENCE_GRAPH_LOC):
         print('ERROR: FROZEN_INFERENCE_GRAPH_LOC "' + FROZEN_INFERENCE_GRAPH_LOC + '" does not seem to exist')
         print('was the inference graph exported successfully?')
         return False
-    # end if
 
     if not os.path.exists(LABELS_LOC):
         print('ERROR: the label map file "' + LABELS_LOC + '" does not seem to exist')
         return False
-    # end if
 
     return True
-# end function
-
 #######################################################################################################################
 if __name__ == "__main__":
     main()
--- STRIP WEBAPP/webapp.py
+++ STRIP WEBAPP/webapp.py
@@ -1,10 +1,13 @@
+# Author: Michael Bridgette
+# Description: Flask backend for my GIP Object detection model.
+# Date of last edit: 03/04/2019
 from flask import Flask, render_template, request, session, jsonify
 import objectDetector
 import mysql.connector
 
 app = Flask(__name__)
 
-
+# Connect to mySql database (Note: FILL IN PASSWORD)
 mydb = mysql.connector.connect(
   host="127.0.0.1",
   user="root",
@@ -12,145 +15,147 @@ mydb = mysql.connector.connect(
   database="results"
 )
 
+# Function that takes mySql records and
+# appends them to a list of results
+def create_list(records):
+    listOfResults = [] # empty list
+    for row in records:
+        result = {
+        'id': row[0],
+        'userId': row[1],
+        'result': row[2],
+        'accuracy': row[3],
+        'photo': row[4].decode('UTF-8'), # decode b64 string
+        'userResult': row[5],
+        'timestamp': row[6]
+        }
+        listOfResults.append(result) # put into the list
+    return listOfResults
 
+
+
+# Home page for demo web application.
 @app.route('/home')
 def get_and_display_form():
     return render_template('index.html',
                            the_title="GIP Strip Result Detection", )
 
 
-
+# This will send JSON containing the last entries in the database. The amount
+# of entries will be based on <int:limit>.
 @app.route('/getHistory/<int:limit>', methods=['GET'])
 def get_history(limit):
     cursor = mydb.cursor()
-    sql_select_Query = "SELECT * FROM strip ORDER BY ts DESC LIMIT " + str(limit)
-    print(sql_select_Query)
-    cursor.execute(sql_select_Query)
+    sql_query = "SELECT * FROM strip ORDER BY ts DESC LIMIT %s" # sql query
+    cursor.execute(sql_query, (limit,))
     records = cursor.fetchall()
 
-    listOfResults = []
+    listOfResults = create_list(records)
 
-    for row in records:
-        result = {
-        'id': row[0],
-        'userId': row[1],
-        'result': row[2],
-        'accuracy': row[3],
-        'photo': row[4].decode('UTF-8'),
-        'userResult': row[5],
-        'timestamp': row[6]
-        }
-        listOfResults.append(result)
     cursor.close()
-    return jsonify(results = listOfResults)
+    return jsonify(results = listOfResults), 201
 
-@app.route('/getresult/<int:user_id>', methods=['GET'])
-def get_result(user_id):
+# This will return JSON for particular entry based on <int:user_id>
+@app.route('/getresult/<int:result_id>', methods=['GET'])
+def get_result(result_id):
     cursor = mydb.cursor()
-    sql_select_query = "select * from strip WHERE id ='" + str(user_id) + "'"
-    cursor.execute(sql_select_query)
+    sql_query = "SELECT * FROM strip WHERE id = %s" # sql_query
+    cursor.execute(sql_query, (result_id,))
     records = cursor.fetchall()
-    listOfResults = []
-    for row in records:
-        result = {
-        'id': row[0],
-        'userId': row[1],
-        'result': row[2],
-        'accuracy': row[3],
-        'photo': row[4].decode('UTF-8'),
-        'userResult': row[5],
-        'timestamp': row[6]
-        }
-        listOfResults.append(result)
-    cursor.close()
-    return jsonify(results = listOfResults)
 
+    listOfResults = create_list(records)
+
+    cursor.close()
+    return jsonify(results = listOfResults), 201
+
+# This will return JSON with all entries for a particular user based on <string:user_name>
 @app.route('/getuserresults/<string:user_name>', methods=['GET'])
 def get_user_results(user_name):
     cursor = mydb.cursor()
-    sql_select_query = "select * from strip WHERE userId ='" + str(user_name) + "'"
-    cursor.execute(sql_select_query)
+    sql_query = "SELECT * FROM strip WHERE userId = %s" #sql query
+    cursor.execute(sql_query, (user_name,))
     records = cursor.fetchall()
-    listOfResults = []
-    for row in records:
-        result = {
-        'id': row[0],
-        'userId': row[1],
-        'result': row[2],
-        'accuracy': row[3],
-        'photo': row[4].decode('UTF-8'),
-        'userResult': row[5],
-        'timestamp': row[6]
-        }
-        listOfResults.append(result)
+
+    listOfResults = create_list(records)
+
     cursor.close()
-    return jsonify(results = listOfResults)
+    return jsonify(results = listOfResults), 201
 
 
-
+# Returns JSON containing Base64 string of the result image and also the accuracy as text
 @app.route('/objectdetection', methods=['POST'])
 def detect_object():
     if not request.json or not 'image' in request.json:
         abort(400)
 
+    # remove the first 23 characters from the string this is necessary
+    # because these characters have to be removed in order for the object detection to work
     file = request.json["image"]
     for x in range(23):
         file = file[1:]
 
 
-    result = objectDetector.Object_Detection(file.encode())
-    result['image'] = result['image'].decode('UTF-8')
-    result['image'] = "data:image/jpeg;base64," + result['image']
-    result['accuracy'] = int(result['accuracy'] * 100)
+    result = objectDetector.Object_Detection(file.encode()) # perform the actual object detection on the image
+    #result['image'] = result['image'].decode('UTF-8') # decode b64 string (result image)
+    result['image'] = "data:image/jpeg;base64," + result['image'] # add this string to beginning of the b64 string
+    result['accuracy'] = int(result['accuracy'] * 100) # result is between 0-1, Im converting it to a percentage for readability
 
     return jsonify(results = result), 201
 
+# Adds new entry to database and returns what was added to the database as JSON
 @app.route('/addEntry/<string:user_name>/<string:user_result>', methods=['POST'])
 def addEntry(user_name, user_result):
     if not request.json or not 'image' in request.json:
         abort(400)
 
+    # remove the first 23 characters from the string this is necessary
+    # because these characters have to be removed in order for the object detection to work
     file = request.json["image"]
     for x in range(23):
         file = file[1:]
 
 
-    result = objectDetector.Object_Detection(file.encode())
-    result['image'] = result['image'].decode('UTF-8')
-    result['image'] = "data:image/jpeg;base64," + result['image']
-    result['accuracy'] = int(result['accuracy'] * 100)
+    result = objectDetector.Object_Detection(file.encode()) # perform the actual object detection on the image
+    #result['image'] = result['image'].decode('UTF-8') # decode b64 string (result image)
+    result['image'] = "data:image/jpeg;base64," + result['image'] # add this string to beginning of the b64 string
+    result['accuracy'] = int(result['accuracy'] * 100) # result is between 0-1, Im converting it to a percentage for readability
 
     cursor = mydb.cursor()
 
-    sql = "INSERT INTO strip (userId,result,accuracy,photo,userResult) VALUES (%s, %s, %s ,%s ,%s)"
+    sql = "INSERT INTO strip (userId,result,accuracy,photo,userResult) VALUES (%s, %s, %s ,%s ,%s)" # sql query
     val = (user_name, result['name'][0].lower(), result['accuracy'], result['image'], user_result[0].lower())
 
     cursor.execute(sql, val)
-    mydb.commit()
+    mydb.commit() # commit change to the database
 
 
     return jsonify(results = result), 201
 
+# Results page of the demo web application
 @app.route('/results', methods=['POST'])
 def process_form():
-    session['file'] = request.form["answer"]
-    session['userName'] = request.form["Username"]
-    session['userResult'] = request.form["UserResult"]
+    session['file'] = request.form["answer"] # get the image from the form
+    session['userName'] = request.form["Username"] # get the username from the form
+    session['userResult'] = request.form["UserResult"] # get the users result from the form
+
+    # remove the first 23 characters from the string this is necessary
+    # because these characters have to be removed in order for the object detection to work
     for x in range(23):
         session['file'] = session['file'][1:]
 
 
-    result = objectDetector.Object_Detection(session['file'].encode())
-    result['image'] = result['image'].decode('UTF-8')
-    result['image'] = "data:image/jpeg;base64," + result['image']
-    result['accuracy'] = int(result['accuracy'] * 100)
+    result = objectDetector.Object_Detection(session['file'].encode()) # perform the actual object detection on the image
+    #result['image'] = result['image'].decode('UTF-8') # decode b64 string (result image)
+    result['image'] = "data:image/jpeg;base64," + result['image'] # add this string to beginning of the b64 string
+    result['accuracy'] = int(result['accuracy'] * 100) # result is between 0-1, Im converting it to a percentage for readability
 
     cursor = mydb.cursor()
 
-    sql = "INSERT INTO strip (userId,result,accuracy,photo,userResult) VALUES (%s, %s, %s ,%s ,%s)"
+    sql = "INSERT INTO strip (userId,result,accuracy,photo,userResult) VALUES (%s, %s, %s ,%s ,%s)" # sql query
     val = (session['userName'], result['name'][0].lower(), result['accuracy'], result['image'], session['userResult'][0].lower())
 
     cursor.execute(sql, val)
+    mydb.commit() # commit change to the database
     return render_template('process.html',
                            the_title="Process Results", the_result = result['image'], the_name = result['name'], the_accuracy = result['accuracy'] )
 
